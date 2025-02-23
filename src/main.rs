398d660f781112b525d7c12cc069defdb6265b36
yusei399 <contact.ikechan@gmail.com>
2025-02-23 23:24:23 +0100

    main追加

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod instructions;
 use crate::instructions::adder::adder;
 use crate::instructions::multiplier::multiplier;
 use crate::instructions::gray_code::gray_code;
-
+use crate::instructions::evaluation::eval_formula;
 fn main() {
     println!("Adder");
     println!("{}", adder(1, 2));
@@ -32,4 +32,53 @@ fn main() {
     println!("{}", gray_code(4));
     println!("{}", gray_code(5));
 
+    println!("Evaluation");
+    println!("Test literal values:");
+    println!("eval_formula(\"1\") = {} (expected: true)", eval_formula("1"));
+    println!("eval_formula(\"0\") = {} (expected: false)", eval_formula("0"));
+
+    // negation
+    println!("Test negation:");
+    println!("eval_formula(\"1!\") = {} (expected: false)", eval_formula("1!"));
+    println!("eval_formula(\"0!\") = {} (expected: true)", eval_formula("0!"));
+
+    // and
+    println!("Test AND:");
+    println!("eval_formula(\"10&\") = {} (expected: false)", eval_formula("10&"));
+    println!("eval_formula(\"11&\") = {} (expected: true)", eval_formula("11&"));
+    println!("eval_formula(\"00&\") = {} (expected: false)", eval_formula("00&"));
+
+    // or -- 正しい RPN 形式なら "01|" となる
+    println!("Test OR:");
+    println!("eval_formula(\"01|\") = {} (expected: true)", eval_formula("01|"));
+    println!("eval_formula(\"00|\") = {} (expected: false)", eval_formula("00|"));
+    println!("eval_formula(\"11|\") = {} (expected: true)", eval_formula("11|"));
+
+    // xor
+    println!("Test XOR:");
+    println!("eval_formula(\"10^\") = {} (expected: true)", eval_formula("10^"));
+    println!("eval_formula(\"11^\") = {} (expected: false)", eval_formula("11^"));
+    println!("eval_formula(\"00^\") = {} (expected: false)", eval_formula("00^"));
+
+    // implication
+    println!("Test implication:");
+    println!("eval_formula(\"10>\") = {} (expected: false)", eval_formula("10>"));
+    println!("eval_formula(\"11>\") = {} (expected: true)", eval_formula("11>"));
+    println!("eval_formula(\"00>\") = {} (expected: true)", eval_formula("00>"));
+    println!("eval_formula(\"01>\") = {} (expected: true)", eval_formula("01>"));
+
+    // equivalence
+    println!("Test equivalence:");
+    println!("eval_formula(\"11=\") = {} (expected: true)", eval_formula("11="));
+    println!("eval_formula(\"10=\") = {} (expected: false)", eval_formula("10="));
+    println!("eval_formula(\"00=\") = {} (expected: true)", eval_formula("00="));
+
+    // complex expression -- RPN では "1011||=" となる
+    println!("Test complex expression:");
+    println!("eval_formula(\"1011||=\") = {} (expected: true)", eval_formula("1011||="));
+
+    // nested negation
+    println!("Test nested negation:");
+    println!("eval_formula(\"1!!\") = {} (expected: true)", eval_formula("1!!"));
+    println!("eval_formula(\"0!!\") = {} (expected: false)", eval_formula("0!!"));
 }
